3c95b022c8eb622d8862f40d5f1e92c87fc52492
Mishranurag07 <99312508+Mishranurag07@users.noreply.github.com>
2022-04-12 10:36:42 +0530

    First Commit

---
+++ LearningJava/src/Unit01/P1_Task01_MyFirstJavaProject.java
@@ -0,0 +1,10 @@
+package Unit01;
+
+public class P1_Task01_MyFirstJavaProject {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!\n");
+		System.out.println("This Is My First Java Program:)");
+	}
+
+}
